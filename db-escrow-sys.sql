5cbbdc140dc932c6323a566fb90550e11ae56028
Abdulloh Fahmi <fahmi@nsiapay.net>
2012-05-17 23:02:32 +0700

    transactions minus transaction s data

--- db-escrow-sys.sql
+++ db-escrow-sys.sql
@@ -62,6 +62,68 @@ payment_channel_redirect_url_3 text,
 constraint pk_payment_channel_id primary key(payment_channel_id));
 
 
-create table payment_method(
-payment_method_id bigint not null,
+create table payment_formula(
+payment_formula_id int not null,
+payment_channel_id bigint not null,
+formula_status boolean,
+formula_start_date date,
+formula_end_date date,
+payment_mdr_fee numeric(6,2),
+payment_debit_fee numeric(10,2),
+payment_escrow_fee numeric(10,2),--service fee
+payment_payment_gateway_fee numeric(6,2),
+payment_fraud_detection_fee numeric(6,2),
+payment_additional_fee_1 numeric(6,2),
+payment_additional_fee_2 numeric(6,2),
+payment_additional_fee_3 numeric(6,2),
+fee_charge_to character(1) default 'M',--M for master merchant H for merchant and C for Customer
+constraint pk_payment_formula_id primary key (payment_formula_id),
+constraint fk_pf_payment_channel_id foreign key(payment_channel_id) references payment_channel(payment_channel_id));
+
+create table master_merchant_formula(
+master_merchant_formula_id bigint not null,
+merchant_connector_id bigint not null,
+payment_formula_id int not null,
+formula_percentage_type character(1),--M for master merchant, H for Merchant
+merchant_share_percentage numeric(6,2) default 0.00,
+master_merchant_share_percentage numeric(6,2) default 0.00,
+master_merchant_formula_status boolean,
+master_merchant_formula_start_date date,
+master_merchant_formula_end_date date,
+constraint pk_master_merchant_formula_id primary key(master_merchant_formula_id),
+constraint fk_mmf_merchant_connector_id foreign key(merchant_connector_id) references merchant_connector(merchant_connector_id),
+constraint fk_mmf_payment_formula_id foreign key(payment_formula_id) references payment_formula(payment_formula_id));
+
+create table transactions(
+transactions_id bigint not null,
+transactions_state character(1),
+transactions_status character(1),
+transactions_time timestamp,
+transactions_done_time timestamp,
+master_merchant_formula_id bigint,
+transactions_amount numeric(12,2),
+user_account_or_cardnumber character varying(100),
+credit_card_no character varying(20),
+card_holder_name character varying(100),
+card_holder_email character varying(100),
+card_holder_city character varying(100),
+card_holder_region character varying(100),
+card_holder_country character varying(100),
+card_holder_phone_no character varying(100),
+card_holder_postal_code character varying(100),
+transactions_vat_amount numeric(12,2),
+transactions_nett_goods numeric(12,2),
+transactions_nett_margins numeric(12,2),
+transactions_amount_1 numeric(12,2),
+transactions_amount_2 numeric(12,2),
+transactions_amount_3 numeric(12,2),
+constraint pk_trx_transactions_id primary key(transactions_id),
+constraint fk_trx_master_merchant_formula_id foreign key(master_merchant_formula_id) references master_merchant_formula(master_merchant_formula_id));
+
+create table transactions_share_data(
+transactions_share_data_id bigint not null,
+transactions_id bigint not null,
+transactions
+
+
 
